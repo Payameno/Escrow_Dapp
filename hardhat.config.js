80be1c81abdc01a1e044a7d896bc6a5d2f6de6d7
Payam Beigi <beigi.payam@gmail.com>
2023-01-07 23:27:28 -0800

    Add goerli testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,20 @@
 require('@nomicfoundation/hardhat-toolbox');
 
+//to deploy to a local blockchain
+// module.exports = {
+//   solidity: "0.8.17",
+//   paths: {
+//     artifacts: "./app/src/artifacts",
+//   }
+// };
+
+//Deploy to the Goerli testnet
 module.exports = {
-  solidity: "0.8.17",
-  paths: {
-    artifacts: "./app/src/artifacts",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    }
   }
 };
